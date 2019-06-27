62a09c674311af9f06e388bfaff5a734864f5d5b
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-27 13:12:54 +0530

    hello...

---
+++ fundamental/src/fundamental/john.java
@@ -0,0 +1,10 @@
+package fundamental;
+
+public class john {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("welcome"+args[0]);
+	}
+
+}
---
+++ fundamental/src/fundamental/string_concatenation.java
@@ -0,0 +1,10 @@
+package fundamental;
+
+public class string_concatenation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(args[0]+" technologies "+args[1]);
+	}
+
+}
